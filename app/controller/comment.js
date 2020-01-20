ac1ca5ee66a9258280e124e8d07683adf57287fb
iMac <imac@iMacdeiMac.local>
2020-01-20 22:00:12 +0800

    新增comment接口调试完成

--- app/controller/comment.js
+++ app/controller/comment.js
@@ -12,13 +12,17 @@ exports.create = async ctx => {
   const token = await ctx.helper.resolveToken(ctx.request.header.authorization.split(' ')[1])
   const uid = token.uid
   ctx.validate(createRule, ctx.request.body);
-  const { content, parents_id, list_id, imgs} = ctx.request.body;
+  const { content, parents_id, list_id, imgs, is_editor} = ctx.request.body;
   const list = await ctx.model.Comment.create({ content, parents_id, list_id, user_id: uid, imgs, is_editor });
-  ctx.model.List.findOne({where: {id: list_id}}).then( list => {
-    list.increment('answer_times').then(res=> {
-      console.log('增加评论次数成功')
+  // 不是给自己评论的视为别人的回答，此时该问题回答数+1
+  if (!is_editor) {
+    ctx.model.List.findOne({where: {id: list_id}}).then( list => {
+      list.increment('answer_times').then(res=> {
+        console.log('增加评论次数成功')
+      })
     })
-  })
+  }
+  
 
   ctx.status = 201;
   ctx.body = {...{list}, ...ctx.helper.jsonSuccess()}
@@ -26,11 +30,11 @@ exports.create = async ctx => {
 
 // get
 exports.index = async ctx => {
-  const {type, page, size} = ctx.query
+  const {type, page, size, list_id} = ctx.query
   
-  const list = await ctx.service.Comment.getList(type, page, size)
+  const comments = await ctx.service.comment.getComment(type, page, size, list_id)
 
-  ctx.body = list
+  ctx.body = comments
 };
 
 // new
--- app/service/comment.js
+++ app/service/comment.js
@@ -5,8 +5,9 @@ function toInt(str) {
   return parseInt(str, 10) || 0;
 }
 
-class ListService extends Service {
-  async getList(type, page, size) {
+class CommentService extends Service {
+  async getComment(type, page, size, list_id) {
+    // type=0不包含自己的评论， type=1包含自己的评论
     type = toInt(type) 
     page = toInt(page)
     size = toInt(size)
@@ -14,33 +15,25 @@ class ListService extends Service {
     const Op = this.app.Sequelize.Op
     const token = await ctx.helper.resolveToken(ctx.request.header.authorization.split(' ')[1])
     const uid = token.uid
-    // 0为未完成list，此时answer_times为0， 不限制user_id； 1为推荐、完成的列表，answer_times非0， 不限制user_id; 2为自我发起的列表，不限制answer_times,限制user_id
+
     const whereSql = [
-      { is_delete: 0, answer_times: 0},
-      { is_delete: 0, answer_times: { [Op.gt]: 0 }, user_id: type == 2 ? uid : { [Op.ne]: null }},
-      { is_delete: 0, user_id: uid}
+      { list_id: list_id, user_id: {[Op.not]: uid}},
+      { list_id: list_id},
     ]
-    const resData = await ctx.model.List.findAndCountAll({
-      attributes: ['id', 'title', 'src_img', 'side_imgs', 'updated_at', 'user_id', 'answer_times'],
+    const resData = await ctx.model.Comment.findAndCountAll({
+      // attributes: ['id', 'title', 'src_img', 'side_imgs', 'updated_at', 'user_id', 'answer_times'],
       where: whereSql[type],
       include: [
         {model: ctx.model.User, attributes: ['id', 'name', 'avatar', 'introduce']},
-        {model: ctx.model.Comment,where: {is_editor: 0, is_delete: 0,}, order: [['updated_at', 'DESC']], limit: 1,
-         include: {
-          model: ctx.model.User},
-          // model: ctx.model.User, where: {id: 2}
-        }
+        // {model: ctx.model.Comment,where: {is_editor: 0, is_delete: 0,}, order: [['updated_at', 'DESC']], limit: 1,
+        //  include: {
+        //   model: ctx.model.User},
+        // }
       ],
       order: [['updated_at', 'DESC']],
       limit: size,
       offset: size * (page -1)
     })
-
-
-  //  const a = resData.findAndCountAll({
-  //     where: {comment: {[Op.ne]: []}}
-  //   })
-
 
     let pagination = {
       total: resData.count,
@@ -53,4 +46,4 @@ class ListService extends Service {
     // return {data: a, meta: {pagination: pagination, type: type}}
   }
 }
-module.exports = ListService;
+module.exports = CommentService;
