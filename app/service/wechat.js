3a4716c30466a61476c68ee9e0722dcbc32b2a26
xuan <xuan@xuandeiMac.local>
2020-06-05 21:35:15 +0800

    完成了扫码看房模块1

--- app/service/wechat.js
+++ app/service/wechat.js
@@ -6,8 +6,8 @@ class WeChatService extends Service {
    * @param status 0或1
    */
   async setFocus(openid, type, status) {
-    const {ctx} = this
-    let data = {is_focus: status, type }
+    const { ctx } = this
+    let data = { is_focus: status, type }
     const wxUser = await this.ctx.curl(`https://api.weixin.qq.com/cgi-bin/user/info?access_token=${this.config.weChat['type']}&openid=${openid}&lang=zh_CN`, {
       dataType: 'json',
       method: 'GET'
@@ -18,25 +18,21 @@ class WeChatService extends Service {
     const resUser = await ctx.service.wechat.creatOrUpdateWechatByUidAndType(openid, type, data)
     return resUser
   }
-  
-  async setUnFocus() {
-
-  }
 
   /** 返回成功或失败的布尔值 */
   async creatOrUpdateWechatByUidAndType(openid, type, data) {
     // 查询wechat数据库是否存在该openid的用户，不存在就新建后执行登录，存在则更新信息后直接执行登录
-    const {ctx} = this
-    const {session_key, refresh_token, access_token, unionid, is_focus} = data // 表里的所有非索引数据
-    let tempObj = {session_key, refresh_token, access_token, unionid, is_focus, type, openid}
+    const { ctx } = this
+    const { session_key, refresh_token, access_token, unionid, is_focus } = data // 表里的所有非索引数据
+    let tempObj = { session_key, refresh_token, access_token, unionid, is_focus, type, openid }
     tempObj = ctx.helper.fliterUndefinedParams(tempObj)
     let res = await ctx.model.Wechat.findOne({
-      where: {user_id: ctx.uid, type, openid}
+      where: { user_id: ctx.uid, type, openid }
     })
     let user = null
     if (res) {
       // 已存在，更新wecaht表
-      await ctx.model.Wechat.update({...tempObj}, {where: {user_id: ctx.uid, type, openid}})
+      await ctx.model.Wechat.update({ ...tempObj }, { where: { user_id: ctx.uid, type, openid } })
       // user = await ctx.model.User.findOne({where: {user_id: res.user_id}})
       return true
     } else {
@@ -47,7 +43,7 @@ class WeChatService extends Service {
         ...tempObj
       }, { transaction })
       // try {
-        
+
       //   await transaction.commit()
       //   return true
       // } catch (error) {
