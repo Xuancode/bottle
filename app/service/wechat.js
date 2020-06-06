324daeea252c9afdd81f2807933c8ce375f4fe4d
xuan <xuan@xuandeiMac.local>
2020-06-06 11:29:39 +0800

    完善关注和取消关注接口12234

--- app/service/wechat.js
+++ app/service/wechat.js
@@ -12,7 +12,7 @@ class WeChatService extends Service {
       dataType: 'json',
       method: 'GET'
     })
-    console.log(wxUser)
+    ctx.logger.info('传来的wxUser: %j', wxUser)
     if (wxUser && wxUser.data && wxUser.data.unionid) {
       data.unionid = wxUser.data.unionid
     }
@@ -24,7 +24,8 @@ class WeChatService extends Service {
 
   async setUnFocus(openid, type, status) {
     const { ctx } = this
-    await ctx.model.Wechat.findOrCreate({ where: { openid }, defaults: { openid, type, is_focus: status } })
+    await ctx.model.Wechat.bulkCreate([{ openid, type, is_focus: status }], { validate: true, updateOnDuplicate: ["type", "is_focus", "updated_at"] })
+    // await ctx.model.Wechat.findOrCreate({ where: { openid }, defaults: { openid, type, is_focus: status } })
   }
 
   /** 返回成功或失败的布尔值 */
