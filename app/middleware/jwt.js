fb9e5c26a4bd14cc372e25fcf8a8f318da1ccf3e
xuan <xuan@xuandeiMac.local>
2020-06-11 18:24:51 +0800

    调试uid完成

--- app/middleware/jwt.js
+++ app/middleware/jwt.js
@@ -2,14 +2,13 @@
  * @Author: xuanpl
  * @Date: 2020-02-18 11:40:47
  * @LastEditors: xuanpl
- * @LastEditTime: 2020-06-11 18:18:07
+ * @LastEditTime: 2020-06-11 18:24:38
  * @Description: jwt中间件
  * @FilePath: /bottle/app/middleware/jwt.js
  */
 module.exports = options => {
   return async function (ctx, next) {
     // 拿到传会数据的header 中的token值
-    ctx.logger.info('进来jwt: %j', '真的进来')
     const token = ctx.request.header.authorization ? ctx.request.header.authorization.split(' ')[1] : null
 
     // 不校验options请求，没想到更好的办法，先用这个
@@ -20,19 +19,15 @@ module.exports = options => {
     } else {
       try {
         const verifyData = await ctx.helper.resolveToken(token)
-        ctx.logger.info('TokenBlacklist前面: %j', verifyData)
         const blackToken = await ctx.model.TokenBlacklist.findOne({ where: { token: token } })
-        ctx.logger.info('TokenBlacklist后面: %j', verifyData)
         if (verifyData.uid && !blackToken) {
           // 注意，还需要补验证
           ctx.uid = verifyData.uid
-          ctx.logger.info('jwt的admin_id是: %j', verifyData.uid)
           await next()
         } else {
           ctx.body = { msg: '登录状态已过期', code: '40100' }
         }
       } catch (e) {
-        console.log('错误', e);
         ctx.logger.info('jwt的错误是: %j', e)
         ctx.body = { msg: '错误', code: '50000' }
       }
