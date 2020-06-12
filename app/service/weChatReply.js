1d3b33270909ef3b308e77570450c9f45592a292
xuan <xuan@xuandeiMac.local>
2020-06-12 17:32:31 +0800

    自动回复可自行设置微信点击列表的功能完成

--- app/service/weChatReply.js
+++ app/service/weChatReply.js
@@ -2,7 +2,7 @@
  * @Author: xuanpl
  * @Date: 2020-06-06 16:33:54
  * @LastEditors: xuanpl
- * @LastEditTime: 2020-06-12 15:00:56
+ * @LastEditTime: 2020-06-12 17:24:22
  * @Description: file content
  * @FilePath: /bottle/app/service/weChatReply.js
  */
@@ -43,24 +43,57 @@ class weChatReplyService extends Service {
         return ''
     }
   }
+  /**
+   * @description: 根据对话框传来的信息返回回复内容；先精确匹配，若没有则进行模糊匹配，匹配到的内容返回供选择问题列表；
+   * 若都没有则返回"啥都没有回什么"对应的内容，若还没有则返回默认回复；
+   * 回复的content都需要检测是否采用了 "send这是最终发送的内容click这是点击的内容end"的格式，若有，则解析成问题列表
+   * @param {string} content 关键词
+   * @return: 对应的回复文字内容
+   */
   async getReplyText(content) {
     const { ctx } = this
+    // 精确匹配
     const resReply = await ctx.model.Reply.findOne({ where: { question: content, state: '启用' } })
     if (resReply) {
-      return resReply.content
+      let content = this.testContent2List(resReply.content)
+      return content
     } else {
+      // 模糊匹配返回问题列表
       const Op = this.app.Sequelize.Op
       const resReplys = await ctx.model.Reply.findAll({ where: { question: { [Op.like]: '%' + content + '%' }, state: '启用' } })
+      if (!resReplys || resReplys.length === 0) {
+        const nullReply = await ctx.model.Reply.findOne({ where: { question: '啥都没有回什么', state: '启用' } })
+        return nullReply ? nullReply.content : '更多服务的完善，还请您提出宝贵意见！'
+      }
       let temp = '为您找到以下相关内容：\n'
       for (let i = 0; i < resReplys.length; i++) {
-        let content = resReplys[i].content
-        content = `${i + 1}、<a href="weixin://bizmsgmenu?msgmenucontent=${content}&msgmenuid=${i + 1}">${content}</a>\n`
-        temp = temp + content
+        let question = resReplys[i].question
+        question = `${i + 1}、<a href="weixin://bizmsgmenu?msgmenucontent=${question}&msgmenuid=${i + 1}">${question}</a>\n`
+        temp = temp + question
       }
       return temp
     }
   }
-
+  /**
+   * @description: 如果内容形如：
+   * 'send这是最终发送的内容click这是点击的内容end'，则替换解析出微信格式如：<a href="weixin://bizmsgmenu?msgmenucontent=测试自动发送&msgmenuid=1">点击</a> 
+   * 若不是，则返回原值
+   * @param {type} content 需要检测的内容
+   * @return: 原字符串或者解析后的微信格式
+   */
+  async testContent2List(content) {
+    const reg = 'send([\\s\\S]*?)click'
+    const arr = content.match(reg)
+    if (!arr) {
+      return content
+    } else {
+      // 直接用替换的方式来做
+      content = content.replace(/send/g, '<a href="weixin://bizmsgmenu?msgmenucontent=')
+      content = content.replace(/click/g, '&msgmenuid=1">')
+      content = content.replace(/end/g, '</a>')
+      return content
+    }
+  }
 }
 
 module.exports = weChatReplyService
